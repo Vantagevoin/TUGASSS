cff30adc286af921f5c21a0cf555b32c4fde17d9
Irushia <Irushia@DESKTOP-8GI82AR>
2022-05-15 23:23:09 +0700

    Add project files.

---
+++ tes/Default.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td colspan="2">
+                        <h1>Insertion/Updation</h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Employee ID</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtEmpID" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Employee First Name</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtEmpFname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Employee Last Name</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtEmpLname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Employee Email-ID</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtEmpEmail" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+
+                        <asp:Button ID="btnInsertion" runat="server" Text="Insert" OnClick="btnInsertion_Click"  />
+                        <asp:Button ID="btnUpdation" runat="server" Text="Update" OnClick="btnUpdation_Click"  />
+                        <asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+
+            <hr />
+
+            <table>
+                <tr>
+                    <td>
+                        <h1>Selection</h1>
+                    </td>
+                </tr>
+                <tr>
+
+                    <td>
+                        <asp:Button ID="btnSelect" runat="server" Text="Select All Data" OnClick="btnSelect_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+                    </td>
+                </tr>
+            </table>
+            <hr />
+            
+            <hr />
+
+            <table>
+                <tr>
+                    <td colspan="3">
+                        <h1>Deletion</h1>
+                    </td>
+                </tr> <tr>
+                    <td>Employee ID</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtEmployeeID" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr >
+
+                    <td colspan="3">
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                        <asp:Label ID="lblmessage" runat="server" ForeColor="Red"></asp:Label>
+
+                    </td>
+                </tr>
+                
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ tes/Default.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Npgsql;
+
+namespace Tugas
+{
+    public partial class Resto : System.Web.UI.Page
+    {
+        protected void btnInsertion_Click(object sender, EventArgs e)
+        {
+            if (SqlDBHelper.ExecuteNonQuery("Insert into customer values(@ID,@Fname,@Lname)", CommandType.Text, new NpgsqlParameter("@ID", Convert.ToInt32(txtEmpID.Text)), new NpgsqlParameter("@Fname", txtEmpFname.Text), new NpgsqlParameter("@Lname", txtEmpLname.Text)))
+            {
+
+                txtEmpFname.Text = ""; txtEmpID.Text = ""; txtEmpLname.Text = "";
+                lblmsg.Text = "Data Has been Saved";
+
+            }
+
+            else
+            {
+                txtEmployeeID.Text = "";
+                lblmessage.Text = "Operasi Gagal";
+
+
+            }
+          
+        }
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            DataTable dt;
+            if (SqlDBHelper.ExecuteDataSet(out dt, "Select * from customer order by id", CommandType.Text))
+            {
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            else
+            {
+               
+
+            }
+
+
+        }
+        protected void btnUpdation_Click(object sender, EventArgs e)
+        {
+            if (SqlDBHelper.ExecuteNonQuery("update customer set id=@ID, nama=@Fname, nama_panggilan=@Lname where id=@ID", CommandType.Text, new NpgsqlParameter("@ID", Convert.ToInt32(txtEmpID.Text)), new NpgsqlParameter("@Fname", txtEmpFname.Text), new NpgsqlParameter("@Lname", txtEmpLname.Text)))
+            {
+
+                txtEmpFname.Text = ""; txtEmpID.Text = ""; txtEmpLname.Text = "";
+                lblmsg.Text = "Data Has been Updated";
+
+            }
+
+            else
+            {
+                txtEmployeeID.Text = "";
+                lblmessage.Text = "Operasi Gagal";
+
+
+            }
+           
+        }
+
+        
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+           
+
+            if (SqlDBHelper.ExecuteNonQuery("Delete from customer where id=@ID", CommandType.Text, new NpgsqlParameter("@ID"  , Convert.ToInt32(txtEmployeeID.Text) <=1)))
+            {
+
+                txtEmployeeID.Text = "";
+                lblmessage.Text = "Data Has been Deleted";
+
+            }
+
+            else
+            {
+                txtEmployeeID.Text = "";
+                lblmessage.Text = "Operasi Gagal";
+
+
+            }
+
+
+        }
+        public static class SqlDBHelper
+        {
+            public static bool ExecuteDataSet(out DataTable dt, string sql, CommandType cmdType, params NpgsqlParameter[] parameters)
+            {
+                using (DataSet ds = new DataSet())
+                using (NpgsqlConnection connStr = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString))
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connStr))
+                {
+                    cmd.CommandType = cmdType;
+                    foreach (var item in parameters)
+                    {
+                        cmd.Parameters.Add(item);
+                    }
+
+                    try
+                    {
+                        cmd.Connection.Open();
+                        new NpgsqlDataAdapter(cmd).Fill(ds);
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        dt = null;
+                        return false;
+
+                    }
+
+
+                    dt = ds.Tables[0];
+                    return true;
+                }
+            }
+            public static bool ExecuteNonQuery(string sql, CommandType cmdType, params NpgsqlParameter[] parameters)
+            {
+                using (DataSet ds = new DataSet())
+                using (NpgsqlConnection connStr = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString))
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connStr))
+                {
+                    cmd.CommandType = cmdType;
+                    foreach (var item in parameters)
+                    {
+                        cmd.Parameters.Add(item);
+                    }
+
+                    try
+                    {
+
+                        cmd.Connection.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        
+                        return false;
+
+                    }
+
+
+                   
+                    return true;
+                }
+            }
+
+
+        }
+    }
+}
